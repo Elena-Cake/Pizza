b02edf5b70aa4265cd33278c7e19fa7f70fc3e70
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-08-31 16:05:50 +0300

    ts fix focus input

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -24,7 +24,7 @@ const Search: React.FC = () => {
     }
 
     const handleDeleteSearchValue = () => {
-        if (inputRef.current) inputRef.current.focus()
+        inputRef.current?.focus()
         dispatch(changeSearchRow(''))
     }
 
